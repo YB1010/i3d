8b833c965f62e45ff701104f42ca46de5a208d73
Yuchen_Yao <s3548974@student.rmit.edu.au>
2018-03-29 20:40:37 +1100

    update

--- assignment1.c
+++ assignment1.c
@@ -1,24 +1,59 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include <GL/gl.h>                                              
-#include <GL/glu.h>
-#include <GL/glut.h>
+#include <Windows.h>
+#include <GL\glew.h>
+#include <GL\freeglut.h>
+#include <iostream>
 
-void display()                                                  
+void display()
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);         
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    /* Draw something here */
+	/* Draw something here */
 
-    glutSwapBuffers();                                          
+	glutSwapBuffers();
+}
+void keyboardCB(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 27:
+	case 'q':
+		exit(EXIT_SUCCESS);
+		break;
+	default:
+		break;
+	}
+}
+void myReshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+void myinit(void)
+{
+}
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(400, 400);
+	glutInitWindowPosition(500, 500);
+	glutCreateWindow("assignment 1");
+	glutKeyboardFunc(keyboardCB);
+	glutReshapeFunc(myReshape);
+	glutDisplayFunc(display);
+	
+
+	myinit();
+
+	glutMainLoop();
 }
 
-int main(int argc, char **argv)                                 
-{
-    glutInit(&argc, argv);                                      
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow("Assignment 1");                             
-    glutDisplayFunc(display);                                   
-    glutMainLoop();                                             
-}
+
