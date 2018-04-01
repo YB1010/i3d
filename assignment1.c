56a1eea0aba4e5d03e86a28e95fb8def307210b2
YB1010 <a627273391@gmail.com>
2018-04-01 23:25:07 +1000

    water

--- assignment1.c
+++ assignment1.c
@@ -1,17 +1,22 @@
 // assignment1.cpp : Defines the entry point for the console application.
 //
-
-#include "stdafx.h"
-
-
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
-#include <Windows.h>
-#include <GL\glew.h>
-#include <GL\freeglut.h>
-#include <iostream>
-#define M_PI 3.1415
+#if _WIN32
+#   include <Windows.h>
+#endif
+#if __APPLE__
+#   include <OpenGL/gl.h>
+#   include <OpenGL/glu.h>
+#   include <GLUT/glut.h>
+#else
+#   include <GL/gl.h>
+#   include <GL/glu.h>
+#   include <GL/glut.h>
+#endif
+
 
 typedef enum { tangent, normal }visualisation;
 
@@ -22,8 +27,27 @@ typedef struct
 } Globals;
 
 Globals global;
+void drawAxes(float length)
+{
+  glBegin(GL_LINES);
+  glColor3f(1,0,0);
+  glVertex3f(0, 0, 0);
+  glVertex3f(length, 0, 0);
+  glEnd();
+  glBegin(GL_LINES);
+  glColor3f(0,1,0);
+  glVertex3f(0, 0, 0);
+  glVertex3f(0, length, 0);
+  glEnd();
+  glBegin(GL_LINES);
+  glColor3f(0,0,1);
+  glVertex3f(0, 0, 0);
+  glVertex3f(0, 0, length);
+  glEnd();
+}
 void displayWater()
 {
+	float y;
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable(GL_BLEND);
 	glBegin(GL_LINES);
@@ -38,15 +62,28 @@ void displayWater()
 	glVertex3f(0.0, 0.0, 1.0);//display of z axis
 	glEnd();
 	glBegin(GL_QUAD_STRIP);
-	glColor4f(0.0, 0.0, 1.0,0.8);//light blue color with transparency
-	glVertex2f(-1.0, -1.0);//v0
-	glVertex2f(-1.0, 0);//v1
-	glVertex2f(0, -1.0);//v2
-	glVertex2d(0, 0);//v3
-	glVertex2f(1.0, -1.0);//v4
-	glVertex2f(1.0, 0);//v5
+	glColor4f(0.0, 1.0, 1.0,0.6);//light blue color with transparency
+
+    float left=-1.0;
+    float right=1.0;
+    float range=right-left;
+    int seg=1000;
+    float stepSize=range/seg;
+
+    for (float x = left; x <= right; x+=stepSize) {
+      float a,b;
+
+      y=0.25*sin(2*M_PI*x);
+      glVertex3f(x, y, 0);
+      glVertex3f(x, -1, 0);
+      
+    
+
+    }
+
+
 	glEnd();
-	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 }
 void display()
 {
