e89abbc5812f0fb56296d9e6db7660022ddeaee4
YB1010 <a627273391@gmail.com>
2018-04-05 07:02:21 +1000

    water's normal and tangent

--- assignment1.c
+++ assignment1.c
@@ -27,12 +27,85 @@ typedef struct
 	visualisation visualisationMode;
 } Globals;
 
+typedef struct { float x, y; } vec2f;
+
 Globals global;
 
 float waterM=0;
 bool waterM_bool=true;
+bool debug_normal=false;
+bool debug_tangent=false;
+bool debug_water=false;
 
 
+void drawVector(float x, float y,float a, float b, float s, bool normalize, float red, float green, float blue)
+{
+	glBegin(GL_LINES);
+	glColor3f(red,green,blue);
+	glVertex3f(x,y,0);
+	if (normalize==true)
+	{
+		float v=sqrt((a*a)+(b*b));
+		a=a/v;
+		b=b/v;
+		a*=s;
+		b*=s;
+		glVertex3f(x+a,y+b,0);
+	}
+	else
+	{
+		a*=s;
+		b*=s;
+		glVertex3f(x+a,y+b,0);
+	}
+
+
+}
+void drawTan(float x, float y, float s, float red, float green, float blue)
+{
+	glBegin(GL_LINES);
+	glColor3f(red,green,blue);
+	float slope;
+    float y1,x1;
+
+    y=0.25*sin(2*M_PI*x+waterM);
+
+    slope=0.25*2*M_PI*cos(2*M_PI*x+waterM);
+    x1=x;
+
+
+  	x1+=s;
+  	y1=slope*(x1-x)+y;
+
+
+
+ 	drawVector(x,y,x1-x,y1-y,1,false,red,green,blue);
+
+ 	glEnd();
+
+}
+void drawNormal(float x, float y, float s, float red, float green, float blue)
+{
+	glBegin(GL_LINES);
+	glColor3f(red,green,blue);
+	float slope;
+    float y1,x1;
+
+    y=0.25*sin(2*M_PI*x+waterM);
+
+    slope=0.25*2*M_PI*cos(2*M_PI*x+waterM);
+    x1=x;
+
+
+  	x1+=s;
+  	y1=(slope)*(x1-x)+y;
+
+
+ 	drawVector(x,y,-(y1-y),x1-x,0.1,true,0,0,1);
+
+
+ 	glEnd();
+}
 
 void drawAxes(float length)
 {
@@ -68,7 +141,22 @@ void displayWater()
 	glVertex3f(0.0, 0.0, 0.0);
 	glVertex3f(0.0, 0.0, 1.0);//display of z axis
 	glEnd();
-	glBegin(GL_QUAD_STRIP);
+
+	glBegin(GL_LINES);
+/*
+	if (debug_water==false)
+		{
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			printf("wireframe off.\n");
+		}
+		else
+		{
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINES);
+			printf("wireframe on.\n");
+		}
+*/
+
+
 	glColor4f(0.0, 1.0, 1.0,0.6);//light blue color with transparency
 
 	float left=-1.0;
@@ -78,17 +166,47 @@ void displayWater()
     int seg=1000;
     float stepSize=range/seg;
 
-    for (float x = left; x <= right; x+=stepSize) {
-      float a,b;
+    
 
-      y=0.25*sin(2*M_PI*x+waterM);
-      glVertex3f(x, y, 0);
-      glVertex3f(x, -1, 0);
+    for (float x = left; x <= right; x+=stepSize) {
+
+	    y=0.25*sin(2*M_PI*x+waterM);
+	    glVertex3f(x, y, 0);
+
+    	glVertex3f(x,-1,0);
+
     }
 
-
 	glEnd();
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	float tx,ty;
+
+	if (debug_tangent==true)
+	{
+		for (float x = left; x <= right; x+=0.05)
+		{
+
+	        y=0.25*sin(2*M_PI*x+waterM);
+			
+		    drawTan(x,y,0.05,1,0,0);
+		}
+
+	}
+
+	if (debug_normal==true)
+	{
+		for (float x = left; x <= right; x+=0.05)
+		{
+			y=0.25*sin(2*M_PI*tx+waterM);
+		    drawNormal(x,y,0.05,0,1,0);
+		}
+	}	
+
+
+			
+	
+
+	
 }
 void updateWater()
 {
@@ -103,9 +221,14 @@ void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glEnable(GL_DEPTH_TEST);
+	
 	/* Draw something here */
 
+	glPolygonMode(GL_BACK, GL_LINES);
+	glPolygonMode(GL_FRONT, GL_LINES);
 	displayWater();
+
+    gluErrorString(glGetError());
 	glutSwapBuffers();
 }
 void keyboardCB(unsigned char key, int x, int y)
@@ -124,6 +247,38 @@ void keyboardCB(unsigned char key, int x, int y)
 		{
 			waterM_bool=false;
 		}
+		break;
+	case 'w':
+		if (debug_water==false)
+		{
+			debug_water=true;
+		}
+		else
+		{
+			debug_water=false;
+		}
+		break;
+	case 'n':
+		if (debug_normal==false)
+		{
+			debug_normal=true;
+		}
+		else
+		{
+			debug_normal=false;
+		}
+		break;
+	case 't':
+		if (debug_tangent==false)
+		{
+			debug_tangent=true;
+		}
+		else
+		{
+			debug_tangent=false;
+		}
+		
+
 	/*case 'n':
 		if (global.visualisationMode == tangent)
 			prin
@@ -150,13 +305,17 @@ void myinit(void)
 }
 int main(int argc, char** argv)
 {
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(400, 400);
 	glutInitWindowPosition(500, 500);
 	glutCreateWindow("assignment 1");
+
 	glutKeyboardFunc(keyboardCB);
+
 	glutReshapeFunc(myReshape);
+
 	glutDisplayFunc(display);
 	
 
